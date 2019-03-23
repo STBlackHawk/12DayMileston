dd1019e3e6c362bcc255c611bc086ae73a013569
Black Hawk <taghikhani.sh@gmail.com>
2019-03-23 19:59:04 -0400

    Changed HTML pages and added session

--- STMilestone.py
+++ STMilestone.py
@@ -11,7 +11,7 @@ import simplejson as json
 import pandas as pd
 from pandas.io.json import json_normalize 
 from bokeh.plotting import figure, output_file, show, save
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect, session
 import datetime
 
 
@@ -20,9 +20,9 @@ import datetime
 
 
 
-
-
 app_STMilestone = Flask(__name__)
+
+
 
 app_STMilestone.prTypes=[]
 colors = ["navy", "green", "purple", "red"]
@@ -44,8 +44,10 @@ cmdict["BA"]="The Boeing Company"
 
 @app_STMilestone.route("/",methods=["GET","POST"])
 def index_lulu():
+    j=0
     if request.method == "GET":
         return render_template("Page1.html")
+        session.clear()
     else:
         app_STMilestone.Company = request.form["Company"]
         app_STMilestone.stDate = request.form["stdate"]
@@ -91,7 +93,7 @@ def index_lulu():
             p = figure(plot_width=800, plot_height=350, x_axis_type="datetime")
             
             # add renderers
-            j=0
+            
             for i in app_STMilestone.prTypes:
                 p.line(dates, stocks[i], color=colors[j], legend=i)
                 j+=1
@@ -107,6 +109,7 @@ def index_lulu():
             save(p)
             
             return redirect("/plot")
+            session.clear()
         
 
 @app_STMilestone.route("/plot")
@@ -119,8 +122,10 @@ def error():
     if request.method == "GET":
         return render_template("error.html")
     else:
+        session.clear()
         return redirect("/")
+        
 
 
 if __name__ == "__main__":
-    app_STMilestone.run(debug=False)
+    app_STMilestone.run(debug=True)
